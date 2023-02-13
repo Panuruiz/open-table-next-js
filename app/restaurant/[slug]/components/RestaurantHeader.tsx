d4b9dd7456fb0dd8b7acf5d74400577eed9635a3
Pablo Núñez Ruiz <pablo@alm.sh>
2023-02-13 18:48:30 +0100

    feat: added the hardcoded components

---
+++ app/restaurant/[slug]/components/RestaurantHeader.tsx
@@ -0,0 +1,16 @@
+import NavBar from "../../../components/NavBar";
+
+const RestaurantHeader = () => {
+  return (
+    <div className="h-96 overflow-hidden">
+      <NavBar />
+      <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+        <h1 className="text-7xl text-white captitalize text-shadow text-center">
+          Milestones Grill (Toronto)
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantHeader;
---
+++ app/restaurant/[slug]/components/RestaurantTitle.tsx
@@ -0,0 +1,9 @@
+const RestaurantTitle = () => {
+  return (
+    <div className="mt-4 border-b pb-6">
+      <h2 className="font-bold text-6xl">Milesstone Grill</h2>
+    </div>
+  );
+};
+
+export default RestaurantTitle;
