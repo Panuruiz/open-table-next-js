296462999e54ece7f5b4670d18f6f44b75633220
Pablo Núñez Ruiz <pablo@alm.sh>
2023-02-16 15:00:10 +0100

    lesson 73: added a middleware before the me endpoint

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -7,24 +7,7 @@ const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const bearerToken = req.headers["authorization"] as string;
-
-  if (!bearerToken) {
-    return res.status(401).json({ errorMessage: "Unauthorized request" });
-  }
-
   const token = bearerToken.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ errorMessage: "Unauthorized request" });
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return res.status(401).json({ errorMessage: "Unauthorized request" });
-  }
 
   const payload = jwt.decode(token) as { email: string };
 
