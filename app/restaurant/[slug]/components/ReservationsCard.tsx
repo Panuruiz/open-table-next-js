2278b02dab8f194b17e4de9b750a0ad49087e290
Pablo Núñez Ruiz <pablo@alm.sh>
2023-02-17 13:22:49 +0100

    lessons 89 - 90: data picker

--- app/restaurant/[slug]/components/ReservationsCard.tsx
+++ app/restaurant/[slug]/components/ReservationsCard.tsx
@@ -1,34 +1,87 @@
 "use client";
 
-const ReservationsCard = () => {
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import { partySize as partySizes, times } from "../../../../data";
+
+type ReservationsCardProps = {
+  openTime: string;
+  closeTime: string;
+};
+
+const ReservationsCard = ({ closeTime, openTime }: ReservationsCardProps) => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+  const handleChangeDate = (date: Date | null) => {
+    if (date) {
+      return setSelectedDate(date);
+    }
+
+    return setSelectedDate(null);
+  };
+
+  const getFilteredTimes = () => {
+    const timesWithinWindow: typeof times = [];
+
+    let isWithinWindow = false;
+
+    times.forEach((time) => {
+      if (time.time === openTime) {
+        isWithinWindow = true;
+      }
+
+      if (isWithinWindow) {
+        timesWithinWindow.push(time);
+      }
+
+      if (time.time === closeTime) {
+        isWithinWindow = false;
+      }
+
+      return timesWithinWindow;
+    });
+
+    return timesWithinWindow;
+  };
+
   return (
     <div className="w-[27%] relative text-reg">
       <div className="fixed w-[15%] bg-white rounded p-3 shadow">
-        <div className="text-center border-b pb-2 font-bold">
-          <h4 className="mr-7 text-lg">Make a Reservation</h4>
+        <div className="pb-2 font-bold text-center border-b">
+          <h4 className="text-lg mr-7">Make a Reservation</h4>
         </div>
-        <div className="my-3 flex flex-col">
+        <div className="flex flex-col my-3">
           <label htmlFor="">Party size</label>
-          <select name="" className="py-3 border-b font-light" id="">
-            <option value="">1 person</option>
-            <option value="">2 people</option>
+          <select name="" className="py-3 font-light border-b" id="">
+            {partySizes.map((size) => (
+              <option value={size.value}>{size.label}</option>
+            ))}
           </select>
         </div>
         <div className="flex justify-between">
           <div className="flex flex-col w-[48%]">
             <label htmlFor="">Date</label>
-            <input type="text" className="py-3 border-b font-light w-28" />
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleChangeDate}
+              className="py-3 font-light border-b text-reg w-28"
+              dateFormat="MMMM d"
+              wrapperClassName="w-[48%]"
+            />
           </div>
           <div className="flex flex-col w-[48%]">
             <label htmlFor="">Time</label>
-            <select name="" id="" className="py-3 border-b font-light">
-              <option value="">7:30 AM</option>
-              <option value="">9:30 AM</option>
+            <select name="" id="" className="py-3 font-light border-b">
+              {getFilteredTimes().map((time) => (
+                <option key={time.time} value={time.time}>
+                  {time.displayTime}
+                </option>
+              ))}
             </select>
           </div>
         </div>
         <div className="mt-5">
-          <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
+          <button className="w-full h-16 px-4 font-bold text-white bg-red-600 rounded">
             Find a Time
           </button>
         </div>
