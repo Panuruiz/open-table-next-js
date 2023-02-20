5d90ed7e0115d3fe8e5105d6a2a5bb9ebac15f06
Pablo Núñez Ruiz <pablo@alm.sh>
2023-02-20 18:33:58 +0100

    fix: restaurant header name style

--- app/restaurant/[slug]/components/RestaurantHeader.tsx
+++ app/restaurant/[slug]/components/RestaurantHeader.tsx
@@ -6,7 +6,7 @@ const RestaurantHeader = ({ title }: RestaurantHeaderProps) => {
   return (
     <div className="overflow-hidden h-96">
       <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-        <h2 className="text-center text-white capitalize text-7xl text-shadow">
+        <h2 className="font-semibold text-center text-white capitalize text-7xl text-shadow">
           {title}
         </h2>
       </div>
